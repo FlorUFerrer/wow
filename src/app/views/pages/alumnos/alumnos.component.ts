8856a5691b92fdc036cb5be5c8b60c82b6df393a
Florencia Ferrer <florurre@gmail.com>
2022-12-04 20:38:06 -0300

    Feat: alumnos

---
+++ src/app/views/pages/alumnos/alumnos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alumnos',
+  templateUrl: './alumnos.component.html',
+  styleUrls: ['./alumnos.component.scss']
+})
+export class AlumnosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
