10e2e8400c25bd15483f184bc090980683ceaa22
Florencia Ferrer <florurre@gmail.com>
2022-12-25 17:09:14 -0300

    delete

--- src/app/pipe/search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchPipe } from "../pipe/search.pipe";
-
-describe("SearchPipe", () => {
-  it("create an instance", () => {
-    const pipe = new SearchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
